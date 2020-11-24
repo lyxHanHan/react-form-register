51d5a0abe33e2f120bbb23437a9f6a2ea741f86e
lyxHanHan <48466003+lyxHanHan@users.noreply.github.com>
2020-11-24 11:04:13 +0800

    complete username and password checkout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,37 @@ import { ERRORS } from "./constants";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+
+    this.state = {
+      ShowUsername: false,
+      ShowPassword: false
+    };
+  }
+
+  handleChangeUsername(e) {
+    if (!ERRORS.username.regex.test(e.target.value)) {
+      this.setState({
+        ShowUsername: true
+      });
+    } else {
+      this.setState({
+        ShowUsername: false
+      });
+    }
+  }
+
+  handleChangePassword(e) {
+    if (!ERRORS.password.regex.test(e.target.value)) {
+      this.setState({
+        ShowPassword: true
+      });
+    } else {
+      this.setState({
+        ShowPassword: false
+      });
+    }
   }
 
   render() {
@@ -25,7 +56,11 @@ export default class App extends React.Component {
               placeholder="6~10 characters"
               type="text"
               noValidate
+              onChange={this.handleChangeUsername}
             />
+            {this.state.ShowUsername && (
+              <span className="label-star">{ERRORS.username.message}</span>
+            )}
           </label>
           <label for="password">
             <span className="label-star">*</span> Password:
@@ -35,7 +70,11 @@ export default class App extends React.Component {
               placeholder="6~10 numbers"
               type="password"
               noValidate
+              onChange={this.handleChangePassword}
             />
+            {this.state.ShowPassword && (
+              <span className="label-star">{ERRORS.password.message}</span>
+            )}
           </label>
           <button type="submit">Register</button>
         </form>
